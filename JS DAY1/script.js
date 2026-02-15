f41aac2f55a597e8b9c036b39f8ce9594a89737a
shivamsingh16268 <shivam@Shivams-MacBook-Air-2.local>
2026-02-15 22:01:31 +0530

    Initial commit: Add JS DAY1 through DAY11 files

---
+++ JS DAY1/script.js
@@ -0,0 +1,17 @@
+// function demo(){
+//     return "hello world";
+// }
+// console.log(demo());
+
+// const demo=()=>"demo"
+// console.log(demo());
+
+
+// function sample(callback){
+//     console.log("sample");
+//     callback()
+// }
+// function test(){
+//     console.log("test")
+// }
+// sample(test)
